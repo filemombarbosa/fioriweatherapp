af1ab3530ecb5a233fbc58d138a38a817b061b3c
Filemom <filemombarbosa@gmail.com>
2021-07-14 13:02:55 -0300

    UPDATE

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,16 +8,153 @@ sap.ui.define([
 
 		return Controller.extend("fioriweatherapp.controller.View1", {
 			onInit: function () {
-				this.teste();
-			},
-
-			checkApiKey: function () {
 				
 			},
 
-			checkServiceAvailability: function () {
-				
+			makeHttpRequest: function (oRequestConfig) {
+				return new Promise(function(resolve, reject) {
+					if (oRequestConfig.sUrlEndPoint) {
+						try {
+							$.ajax({
+								url: oRequestConfig.sUrlEndPoint,
+								method: oRequestConfig.sMethod || "GET",
+								dataType: "JSON",
+								async: true,
+								timeout: oRequestConfig.sWaitForResponseMaxTime || 60000,
+								crossDomain: true,
+								processData: false,
+								contentType: false,
+								beforeSend: function () {
+									sap.ui.core.BusyIndicator.show();
+								},
+								complete: function () {
+									sap.ui.core.BusyIndicator.hide();
+								},
+								error: function (response) {
+									reject(response);
+								},
+								success: (oResponse) => {
+									resolve(oResponse);									
+								}								
+							});				
+						} catch (error) {
+							console.error('Error during the HTTP request');
+							reject(sApiKeyIsValid);
+						}
+					} else {
+						console.error('URL para requisição não informada');
+						reject();
+					} 
+				});
 			},
+
+			getApiKey: function () {
+				var sApiKeyFound 		= false;
+				var sApiConfigFilePath 	= jQuery.sap.getModulePath("fioriweatherapp", "/config/openweathermap.json"); 
+				var oModel 				= new JSONModel(sApiConfigFilePath);
+
+				return new Promise(function(resolve, reject) {
+					try {
+						oModel.attachRequestCompleted(function(event) {
+							var sApiKey = event.getSource().getData().apiKey || false;
+
+							if (sApiKey && sApiKey!= "") {
+								resolve(sApiKey);
+							}
+
+							reject(sApiKeyFound);							
+						});
+					} catch (error) {
+						console.error('Error during the API Key search');
+						reject(sApiKeyFound);
+					}
+				});
+			},
+
+			checkServiceAvailability: function (sWaitForResponseMaxTime) {
+				var getApiKey 			= this.getApiKey;
+				var makeHttpRequest 	= this.makeHttpRequest;
+				var showErrorCaseByStatusCode = this.showErrorCaseByStatusCode;
+				
+				var sServiceIsAvailable = false;
+
+				return new Promise(function(resolve, reject) {
+					try {
+						getApiKey()
+						.then(sApiKey => {
+							var oRequestConfig = {
+								sUrlEndPoint: `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${sApiKey}`,
+								sMethod: "POST", 
+								sWaitForResponseMaxTime: sWaitForResponseMaxTime
+							}
+							
+							return makeHttpRequest(oRequestConfig);																			
+						})
+						.then(response => {
+							if (response) {								
+								sServiceIsAvailable = true;
+							}
+													
+							resolve(sServiceIsAvailable);
+						})
+						.catch(responseError => {
+							if (responseError && responseError.status) {
+								showErrorCaseByStatusCode(responseError.status);								
+							}	
+
+							reject(sServiceIsAvailable);	
+						})						
+					} catch (error) {
+						console.error('Error during service availability check');
+						reject(sServiceIsAvailable);
+					}
+				});
+			},
+
+			checkApiKey: function (sWaitForResponseMaxTime) {
+				var getApiKey 		= this.getApiKey;
+				var makeHttpRequest = this.makeHttpRequest;
+				var sApiKeyIsValid 	= false;
+
+				return new Promise(function(resolve, reject) {
+					try {
+						getApiKey()
+						.then(sApiKey => {
+							var oRequestConfig = {
+								sUrlEndPoint: `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${sApiKey}`,
+								sMethod: "POST", 
+								sWaitForResponseMaxTime: sWaitForResponseMaxTime
+							}
+							
+							return makeHttpRequest(oRequestConfig);																			
+						})
+						.then(response => {
+							if (response) {								
+								sApiKeyIsValid = true;
+							}
+							
+							resolve(sApiKeyIsValid);
+						})
+						.catch(responseError => {
+							if (responseError && responseError.responseText) {
+								console.error(responseError.responseText);
+							}
+
+							console.error('Error during the API Key check');
+							reject(sApiKeyIsValid);
+						})
+					} catch (error) {
+						console.error('Error during the API Key check');
+						reject(sApiKeyIsValid);
+					}
+				});
+			},
+
+			
+			
+			checkCityName: function () {
+				
+			},						
 
 			getTilesModel: function () {
 				
@@ -29,11 +166,7 @@ sap.ui.define([
 
 			getWeatherForecast: function () {
 				
-			},
-
-			makeHttpRequest: function () {
-				
-			},
+			},		
 
 			handleWeatherForecastFailer: function () {
 				
@@ -42,6 +175,37 @@ sap.ui.define([
 			handleWeatherForecastExeption: function () {
 				
 			},
+
+			showErrorCaseByStatusCode: function (status) {
+				switch (status) {
+					case 401:
+						console.error('Error during the service check. Please check the following cases:');
+						console.group();
+							console.error('You did not specify your API key in API request.');
+							console.error('Your API key is not activated yet. Within the next couple of hours, it will be activated and ready to use.');
+							console.error('You are using wrong API key in API request. Please, check your right API key in personal account.');
+							console.error('You have free subscription and try to get access to our paid services (for example, 16 days/daily forecast API, any historical weather data, Weather maps 2.0, etc). Please, check your tariff in your personal account and pay attention at our price and condition.');
+						break;
+					case 401:
+						console.error('Error during the service check. Please check the following cases:');
+						console.group();
+							console.error('You make a wrong API request. Please, check your API request.');
+							console.error('You specify wrong city name, ZIP-code or city ID.');
+						break;	
+					case 401:
+						console.error('Error during the service check. Please check the following cases:');
+						console.group();
+							console.error('You make a wrong API request. Please, check your API request.');
+							console.error('You specify wrong city name, ZIP-code or city ID.');
+						break;		
+				}
+			},
+
+
+
+
+
+
 
 			teste: function () {
 				var teste = 1 + 2;
--- webapp/test/unit/controller/View1.controller.js
+++ webapp/test/unit/controller/View1.controller.js
@@ -7,34 +7,87 @@ sap.ui.define([
 
 	QUnit.module("Unit tests for View1 Controller");
 
-	QUnit.test("It should call the init method", function (assert) {
+	QUnit.test("It should call the init method no problems", function (assert) {
 		var oController = new Controller();
 		oController.onInit();
 		assert.ok(oController);
 	});
 
-	QUnit.test("I should find all main functions", function (assert) {
+	QUnit.test("It should find all main functions", function (assert) {
 		var oController 		= new Controller();
 		var oControllerMethod 	= oController.getMetadata().getAllMethods();
 		var sAllFunctionsExist 	= true;
-		var aMainFunctionList 	= [			
-			'checkApiKey',
-			'checkServiceAvailability',			
+		var aNotFounMethods 	= [];
+		var aMainFunction 		= [	
+			'makeHttpRequest',		
+			'getApiKey',
+			'checkServiceAvailability',
+			'checkApiKey',			
+			'checkCityName',
 			'getTilesModel',
 			'setTilesModel',			
-			'getWeatherForecast',
-			'makeHttpRequest',
+			'getWeatherForecast',			
 			'handleWeatherForecastFailer',
 			'handleWeatherForecastExeption',
+			'showErrorCaseByStatusCode',
 		];
 		
-		aMainFunctionList.forEach(sFunctionName => {
+		aMainFunction.forEach(sFunctionName => {
 			if(!oControllerMethod.hasOwnProperty(sFunctionName)){
+				aNotFounMethods.push(sFunctionName);
 				sAllFunctionsExist = false;
 			}			 
-		})
+		});	
+		
+		if (aNotFounMethods.length > 0) {
+			console.error('Not found methods: ' + aNotFounMethods)
+		}
 
-		assert.ok(sAllFunctionsExist);
+		assert.ok(sAllFunctionsExist);		
 	});
 
+	QUnit.test("It should find the Weather API Key", function (assert) {
+		var done 		= assert.async();
+		var oController = new Controller();
+
+		oController.getApiKey()
+		.then(result => {
+			assert.ok(result);
+			done();
+		})
+		.catch(function(err) {
+			console.error('Error found in the promise function');
+			done();
+		});
+	});
+
+	QUnit.test("It should validate the Weather API Key", function (assert) {
+		var done 		= assert.async();
+		var oController = new Controller();
+
+		oController.checkApiKey(10000)
+		.then(result => {
+			assert.ok(result);
+			done();
+		})
+		.catch(function(err) {
+			console.error('Error found in the API Key check');
+			done();
+		});
+	});
+
+	QUnit.test("It should check the Weather API Key service status", function (assert) {
+		var done 		= assert.async();
+		var oController = new Controller();
+
+		oController.checkServiceAvailability(10000)
+		.then(result => {
+			assert.ok(result);
+			done();
+		})
+		.catch(function(err) {
+			console.error('Error found in the API service check');
+			done();
+		});
+	});
 });
